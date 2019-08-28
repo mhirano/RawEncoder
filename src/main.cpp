98138cc83a2df8f8260d718fe3e2ebd42c328772
mhirano <Masahiro_Hirano@ipc.i.u-tokyo.ac.jp>
2019-08-28 18:47:18 +0900

    skip already encoded files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <filesystem> // checked on Ubuntu
+#include <algorithm>
 
 using namespace cv;
 using namespace std;
@@ -34,15 +35,26 @@ int main(int argc, char **argv) {
     static int frameHeight = 600;
 
     std::vector<std::string> rawFileArray;
-    std::vector<std::string> parentPathFileArray;
+    std::vector<std::string> encodedFileArray;
 
     for(const fs::directory_entry& x : fs::recursive_directory_iterator(rootDirString)){
         std::string extension = x.path().extension().string();
-        if(extension == ".raw"){
-            parentPathFileArray.push_back(x.path().parent_path().string());
-            rawFileArray.push_back(x.path().stem().string());
+        if (extension == ".raw"){
+            rawFileArray.push_back(x.path().parent_path().string() + "/" + x.path().stem().string());
+            // std::cout << ".raw file: " << rawFileArray.back() << ".raw" << std::endl;
+        } else if(extension == ".avi"){
+            encodedFileArray.push_back(x.path().parent_path().string() + "/" + x.path().stem().string());
+            // std::cout << ".avi file: " << encodedFileArray.back() << ".avi" << std::endl;
         }
     }
+
+    // for(const fs::directory_entry& x : fs::recursive_directory_iterator(rootDirString)){
+    //     std::string extension = x.path().extension().string();
+    //     if(extension == ".raw"){
+    //         //parentPathFileArray.push_back(x.path().parent_path().string());
+    //         //rawFileArray.push_back(x.path().stem().string());
+    //     }
+    // }
 
     int numFile = rawFileArray.size();
 
@@ -50,14 +62,25 @@ int main(int argc, char **argv) {
 //#pragma omp parallel for // サーバからの読み込みが律速になっているので早くならないどころかランダムアクセス度が高まるので遅くなる
 //#endif
     for(int i = 0; i<numFile; i++){
-        std::string fileStem = rawFileArray[i];
-        std::string parentPath = parentPathFileArray[i];
-        printf("Encoding %s/%s.raw in thread num: %d\n",
-                parentPath.c_str(), fileStem.c_str(), omp_get_thread_num());
+        //std::string fileStem = rawFileArray[i];
+        //std::string parentPath = parentPathFileArray[i];
+        std::string rawFilePath = rawFileArray[i];
+        printf("%s.raw", rawFilePath.c_str());
+        auto rawItr = find(encodedFileArray.cbegin(), encodedFileArray.cend(), rawFilePath);
+        if(rawItr != encodedFileArray.cend()){
+            printf("...skipped\n");
+        } else{
+            printf("...encode\n");
+            printf("Encoding %s.raw in thread num: %d\n",
+                   rawFilePath.c_str(), omp_get_thread_num());
 
-        VideoManager vm(frameWidth, frameHeight);
-        vm.loadVideoFromFile(parentPath + "/" + fileStem+ ".raw");
-        vm.encodeToAVI(parentPath + "/" + fileStem + ".avi", 10.0, 25);
+            VideoManager vm(frameWidth, frameHeight);
+            vm.loadVideoFromFile(rawFilePath + ".raw");
+            vm.encodeToAVI(rawFilePath + ".avi", 10.0, 25);
+        }
+        printf("\n");
+
+
     }
 
     return 0;
